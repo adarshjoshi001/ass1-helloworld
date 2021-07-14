cc91ea1b3821916bdb42b71c7e9917302d4057f0
ADARSH JOSHI <84890549+adarshjoshi001@users.noreply.github.com>
2021-07-14 16:44:32 +0530

    Add files via upload

---
+++ src/Calc.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calc  = () =>{
+    return( <>
+    <h1 style = {{textAlign : "center"}}  > Hello World</h1>
+    <h1 style = {{textAlign : "center"}}  > This is Adarsh Joshi and this is made for lets upgrade</h1>
+    </>
+    )
+}
+
+export default Calc;
